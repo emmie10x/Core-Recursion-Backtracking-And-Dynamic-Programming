2c1ef174005f6387c8b0a61e2574fd585691702f
emmie10x <ssingh03604@gmail.com>
2026-02-28 23:39:14 +0530

    Print Decreasing Number Soution in c++

---
+++ Recursion/DecNumber.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int decrease(int n){
+    if(n==0) return 0;
+    cout<<n<<" ";
+    return decrease(n-1);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    decrease(n);
+    return 0;
+}
